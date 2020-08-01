b3253a041b561a34c954718d610a42bcadb76130
Michael Graps <michaelthegraps@gmail.com>
2020-08-01 12:04:42 +1000

    plot data in pysimplegui

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import PySimpleGUI as gui
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 layout = [[gui.Text('Document to open')],
           [gui.In(), gui.FileBrowse()],
-          [gui.Open(), gui.Cancel()]]
+          [ gui.Button('plot'), gui.Cancel()]]
 
 window = gui.Window('Window that stays open', layout)
+
+def draw_plot(volts, amps):
+    plt.plot(volts, amps)
+    plt.show(block=False)
 
 while True:
     event, values = window.read()
 
     fname = values[0]
-    gui.popup('The filename you chose was', fname)
+    data = pd.read_csv(fname, header=None, delimiter=r"\s+")
+    volts = data.iloc[:,0].to_numpy()
+    amps = data.iloc[:,1].to_numpy()
+    
+    
 
     if event == gui.WIN_CLOSED or event == 'Exit':
         break
+    elif event == 'plot':
+        draw_plot(volts, amps)
 
 window.close()
