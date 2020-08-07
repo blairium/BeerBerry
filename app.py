8dfd9c1e6718877e8e7f111dd476e50cb783fe06
Michael <michaelthegraps@gmail.com>
2020-08-07 22:29:11 +1000

    gui read and write binary

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import NateFunc as nFunc
 
 layout = [[gui.Text('Document to open')],
           [gui.In(), gui.FileBrowse()],
-          [ gui.Button('plot'), gui.Cancel()]]
+          [ gui.Button('plot'), gui.Button('save'), gui.Cancel()]]
 
 window = gui.Window('Window that stays open', layout)
 
@@ -23,11 +23,15 @@ def get_time_values(volts):
     t = n*dt
     return t
 
+def save_bin(df):
+    df.to_pickle("myfile.bin")
+
 while True:
     event, values = window.read()
 
     fname = values[0]
     data = pd.read_csv(fname, header=None, delimiter=r"\s+")
+    #data = pd.read_pickle(fname)
     volts = data.iloc[:,0].to_numpy()
     amps = data.iloc[:,1].to_numpy()
     time = get_time_values(volts)
@@ -38,5 +42,7 @@ while True:
         break
     elif event == 'plot':
         draw_plot(time, amps)
+    elif event == 'save':
+        save_bin(data)
 
 window.close()
