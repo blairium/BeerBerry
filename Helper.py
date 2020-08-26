5f811b8a1e2d404cc3dfb8d85fb749957d7f626a
Nyuk <gillbanks.nate@gmail.com>
2020-08-26 17:55:21 +1000

    (Save/Load) (Pre/Post) Added
    Added saving and loading of both pre and post calculation

--- Helper.py
+++ Helper.py
@@ -1,34 +1,66 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import os as os
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 matplotlib.use('TkAgg')
+
 
 def binary_Write(fileName, df):
     df.to_pickle(fileName)
 
+
+def binary_Write_Post(fileName, df):
+    df.to_pickle(fileName)
+
+
 def csv_Write(fileName, df):
-    df.to_csv(fileName)
+    df.to_csv(fileName, index=False, sep=' ')
+
+
+def csv_WritePost(fileName, df):
+    df.to_csv(fileName, index_label='row')
+    # columns=['t', 'i', 'Imag', 'Imagfilt', 'ifilt', 'ienv', 'int_ienv', 'ienv_filtered']
+
 
 def binary_Read(fileName):
     data = pd.read_pickle(fileName)
     return data
 
+
+def binary_Read_Post(fileName):
+    data = pd.read_pickle(fileName)
+    return data
+
+
+def data_read_Post(fileName):
+    data = pd.read_csv(fileName)
+    return data
+
+
 def data_read(fileName):
     data = pd.read_csv(fileName, header=None, delimiter=r"\s+")
     return data
 
+
 def csv_read(fileName):
     data = pd.read_csv(fileName, header=None)
-    data = data.iloc[:,1:]
+    data = data.iloc[:, 1:]
     return data
+
+
+def csv_read_Post(fileName):
+    data = pd.read_csv(fileName, header=0)
+    data = data.iloc[:, 1:]
+    return data
+
 
 def draw_plot(time, amps):
     plt.plot(time, amps)
     plt.show(block=False)
+
 
 def get_time_values(volts):
     sample_rate = 8000.0
@@ -38,38 +70,51 @@ def get_time_values(volts):
     t = n * dt
     return t
 
+
 def draw_plot(time, amps):
     plt.plot(time, amps)
     plt.show(block=False)
 
-def readFile(fileName):
 
+def readFile(fileName, post):
     ext = os.path.splitext(fileName)[-1].lower()
-    if ext == ".data":
-        data = data_read(fileName)
-    elif ext == ".bin":
+
+    if post == 1:
+
+        if ext == ".data":
+            data = data_read_Post(fileName)
+        elif ext == ".bin":
+            data = binary_Read_Post(fileName)
+        elif ext == ".csv":
+            data = csv_read_Post(fileName)
+
+    else:
+        if ext == ".data":
+            data = data_read(fileName)
+        elif ext == ".bin":
             data = binary_Read(fileName)
-    elif ext == ".csv":
+        elif ext == ".csv":
             data = csv_read(fileName)
 
     return data
 
-def writeFile(fileName, data):
+
+def writeFile(fileName, data, post):
     ext = os.path.splitext(fileName)[-1].lower()
-    if ext == ".data":
-        csv_Write(fileName, data)
-    elif ext == ".bin":
-        binary_Write(fileName, data)
-    elif ext == ".csv":
-        csv_Write(fileName, data)
+    ##
+    if post == 1:
 
+        if ext == ".data":
+            csv_WritePost(fileName, data)
+        elif ext == ".bin":
+            binary_Write_Post(fileName, data)
+        elif ext == ".csv":
+            csv_WritePost(fileName, data)
 
-
-
-
-
-
-
-
-
-
+    elif post == 0:
+        if ext == ".data":
+            csv_Write(fileName, data)
+        elif ext == ".bin":
+            binary_Write(fileName, data)
+        elif ext == ".csv":
+            csv_Write(fileName, data)
--- app.py
+++ app.py
@@ -11,27 +11,31 @@ from matplotlib.figure import Figure
 from data.And_AC_Volt_Python import major_function
 from matplotlib.widgets import PolygonSelector
 from matplotlib.axes import Axes
+
 matplotlib.use('TkAgg')
 
-sg.LOOK_AND_FEEL_TABLE['BeerBerry'] =  {'BACKGROUND': '#FFFFFF',
-                                        'TEXT': '#000000',
-                                        'INPUT': '#C4C4C4',
-                                        'TEXT_INPUT': '#000000',
-                                        'SCROLL': '#c7e78b',
-                                        'BUTTON': ('white', '#40BAD2'),
-                                        'PROGRESS': ('#01826B', '#D0D0D0'),
-                                        'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
-                                        }
+sg.LOOK_AND_FEEL_TABLE['BeerBerry'] = {'BACKGROUND': '#FFFFFF',
+                                       'TEXT': '#000000',
+                                       'INPUT': '#C4C4C4',
+                                       'TEXT_INPUT': '#000000',
+                                       'SCROLL': '#c7e78b',
+                                       'BUTTON': ('white', '#40BAD2'),
+                                       'PROGRESS': ('#01826B', '#D0D0D0'),
+                                       'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
+                                       }
 PASSWORD = '1245'
 PARAMETERS_FILE = path.join(path.dirname(__file__), r'parameters_file.cfg')
-DEFAULT_SETTINGS = {'freq_pert': 60, 'bandwith_window': 10 , 'lpf_bw': 10, 'harmonic' : 2, 'max_time' : 1.5,
-                    'max_width' : 0.2, 'sample_rate': 8000.0, 'theme': sg.theme('BeerBerry')}
+DEFAULT_SETTINGS = {'freq_pert': 60, 'bandwith_window': 10, 'lpf_bw': 10, 'harmonic': 2, 'max_time': 1.5,
+                    'max_width': 0.2, 'sample_rate': 8000.0, 'theme': sg.theme('BeerBerry')}
 # "Map" from the settings dictionary keys to the window's element keys
-PARAMETER_KEYS_TO_ELEMENT_KEYS = {'freq_pert': '-FREQ PERT-', 'bandwith_window': '-BW WINDOW-' ,'lpf_bw': '-LPF BW-', 'harmonic': '-HARMONIC-',
-                                'max_time': '-MAX TIME-', 'max_width': '-MAX WIDTH-','sample_rate': '-SAMPLE RATE-','theme': '-THEME-' }
+PARAMETER_KEYS_TO_ELEMENT_KEYS = {'freq_pert': '-FREQ PERT-', 'bandwith_window': '-BW WINDOW-', 'lpf_bw': '-LPF BW-',
+                                  'harmonic': '-HARMONIC-',
+                                  'max_time': '-MAX TIME-', 'max_width': '-MAX WIDTH-', 'sample_rate': '-SAMPLE RATE-',
+                                  'theme': '-THEME-'}
 
 xdata = []
 ydata = []
+
 
 ###### Load/Save Parameters File ##########################################
 def load_parameters(parameters_file, default_parameters):
@@ -39,13 +43,15 @@ def load_parameters(parameters_file, default_parameters):
         with open(parameters_file, 'r') as f:
             parameters = jsonload(f)
     except Exception as e:
-        sg.popup_quick_message(f'exception {e}', 'No parameters file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+        sg.popup_quick_message(f'exception {e}', 'No parameters file found... will create one for you',
+                               keep_on_top=True, background_color='red', text_color='white')
         parameters = default_parameters
         save_parameters(parameters_file, parameters, None)
     return parameters
 
+
 def save_parameters(parameters_file, parameters, values):
-    if values:      # if there are stuff specified by another window, fill in those values
+    if values:  # if there are stuff specified by another window, fill in those values
         for key in PARAMETER_KEYS_TO_ELEMENT_KEYS:  # update window with the values read from settings file
             try:
                 parameters[key] = values[PARAMETER_KEYS_TO_ELEMENT_KEYS[key]]
@@ -57,68 +63,80 @@ def save_parameters(parameters_file, parameters, values):
 
     sg.popup('Parameters saved')
 
+
 def onclick(event):
-    #print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-     #     ('double' if event.dblclick else 'single', event.button,
-      #     event.x, event.y, event.xdata, event.ydata))
+    # print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+    #     ('double' if event.dblclick else 'single', event.button,
+    #     event.x, event.y, event.xdata, event.ydata))
     if (len(xdata) < 2):
         xdata.append(event.xdata)
         ydata.append(event.ydata)
         print(xdata)
         print(ydata)
 
-def draw_figure(canvas, figure, toolbar = None):
+
+def draw_figure(canvas, figure, toolbar=None):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     toolbar = NavigationToolbar2Tk(figure_canvas_agg, canvas)
-    
-    
+
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-   
-    
 
     return figure_canvas_agg, toolbar
 
-def destroy_figure(fig_canvas_agg, toolbar):
-        fig_canvas_agg.get_tk_widget().forget()
-        toolbar.forget()
-        plt.close('all')
 
-#ab1 = [[sg.Canvas(size = (700,500), key='-CANVAS-')],
+def destroy_figure(fig_canvas_agg, toolbar):
+    fig_canvas_agg.get_tk_widget().forget()
+    toolbar.forget()
+    plt.close('all')
+
+
+# ab1 = [[sg.Canvas(size = (700,500), key='-CANVAS-')],
 #       [sg.Button('baseline')]]
-#tab2 = [[sg.Canvas(size = (700,500), key='-CANVAS2-')]]
-#tab3 = [[sg.Canvas(size = (700,500), key='-CANVAS3-')]]
+# tab2 = [[sg.Canvas(size = (700,500), key='-CANVAS2-')]]
+# tab3 = [[sg.Canvas(size = (700,500), key='-CANVAS3-')]]
 
 ####### Creating the Main Window ################################
 def create_main_window(parameters, password_attempt):
     sg.theme(parameters['theme'])
-    layout = [[sg.In(), sg.FileBrowse(), sg.Button('Log in', visible = False if password_attempt==PASSWORD else True), sg.Button('Logout', visible = True if password_attempt==PASSWORD else False)],
-              [sg.Button('Load'), sg.Button('Insert Parameters', visible = True if password_attempt==PASSWORD else False)],
-              [sg.Canvas(size = (898,634), key='-CANVAS-')],
-              [sg.Button('plot', disabled=True,), sg.Button('plot2', disabled=True,), sg.Button('plot3', disabled=True,), sg.Button('baseline', disabled=True,),
-               sg.FileSaveAs(button_text='save', disabled=True, target='save', enable_events=True, key='save', file_types=(('DATA', '.data'), ('BIN', '.bin'), ('CSV', '.csv'), ('All Files', '*.*'))),
+    layout = [[sg.In(), sg.FileBrowse(), sg.Button('Log in', visible=False if password_attempt == PASSWORD else True),
+               sg.Button('Logout', visible=True if password_attempt == PASSWORD else False)],
+              [sg.Button('Load'),
+               sg.Button('Insert Parameters', visible=True if password_attempt == PASSWORD else False)],
+              [sg.Radio('Pre-Calc', 'RAD1', default=True, font=['Helvetica', 10], key='OP1'),
+               sg.Radio('Post-Calc', 'RAD1', font=['Helvetica', 10])],
+              [sg.Canvas(size=(898, 634), key='-CANVAS-')],
+              [sg.Radio('Pre-Calc', 'RAD2', default=True, font=['Helvetica', 10], key='OP2'),
+               sg.Radio('Post-Calc', 'RAD2', font=['Helvetica', 10])],
+              [sg.Button('plot', disabled=True, ), sg.Button('plot2', disabled=True, ),
+               sg.Button('plot3', disabled=True, ), sg.Button('baseline', disabled=True, ),
+               sg.FileSaveAs(button_text='save', disabled=True, target='save', enable_events=True, key='save',
+                             file_types=(('DATA', '.data'), ('BIN', '.bin'), ('CSV', '.csv'), ('All Files', '*.*'))),
                sg.Button('Exit')]]
 
     return sg.Window('BeerBerry', layout, element_justification='center', font='Helvetica 18')
 
+
 ####### Creating parameters window ##############################
 def create_insert_parameters_window(parameters):
     sg.theme(parameters['theme'])
-    def TextLabel(text): return sg.Text(text+':', justification='r', size=(30,1))
 
-    layout = [  [sg.Text('Parameters', justification='center', font='Helvetica 18')],
-                [TextLabel('Frequency perturbation'), sg.Input(key='-FREQ PERT-')],
-                [TextLabel('Band-width window'),sg.Input(key='-BW WINDOW-')],
-                [TextLabel('Env lpf bandwidth'),sg.Input(key='-LPF BW-')],
-                [TextLabel('Harmonic to Use'),sg.Input(key='-HARMONIC-')],
-                [TextLabel('Maximum Time'),sg.Input(key='-MAX TIME-')],
-                [TextLabel('Maximum Width'),sg.Input(key='-MAX WIDTH-')],
-                [TextLabel('Sample Rate'),sg.Input(key='-SAMPLE RATE-')],
-                [TextLabel('Theme'),sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
-                [sg.Button('Save'), sg.Button('Exit')]  ]
+    def TextLabel(text):
+        return sg.Text(text + ':', justification='r', size=(30, 1))
+
+    layout = [[sg.Text('Parameters', justification='center', font='Helvetica 18')],
+              [TextLabel('Frequency perturbation'), sg.Input(key='-FREQ PERT-')],
+              [TextLabel('Band-width window'), sg.Input(key='-BW WINDOW-')],
+              [TextLabel('Env lpf bandwidth'), sg.Input(key='-LPF BW-')],
+              [TextLabel('Harmonic to Use'), sg.Input(key='-HARMONIC-')],
+              [TextLabel('Maximum Time'), sg.Input(key='-MAX TIME-')],
+              [TextLabel('Maximum Width'), sg.Input(key='-MAX WIDTH-')],
+              [TextLabel('Sample Rate'), sg.Input(key='-SAMPLE RATE-')],
+              [TextLabel('Theme'), sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
+              [sg.Button('Save'), sg.Button('Exit')]]
 
     window = sg.Window('Insert Parameters', layout, keep_on_top=True, finalize=True)
 
-    for key in PARAMETER_KEYS_TO_ELEMENT_KEYS:   # update window with the values read from settings file
+    for key in PARAMETER_KEYS_TO_ELEMENT_KEYS:  # update window with the values read from settings file
         try:
             window[PARAMETER_KEYS_TO_ELEMENT_KEYS[key]].update(value=parameters[key])
         except Exception as e:
@@ -126,17 +144,19 @@ def create_insert_parameters_window(parameters):
 
     return window
 
+
 # Initialising empty variables so they can remain within the whole program scope
-t,i,f,Imag,Imagfilt,ifilt,ienv,int_ienv,ienv_filtered = ([] for i in range(9))
+t, i, f, Imag, Imagfilt, ifilt, ienv, int_ienv, ienv_filtered = ([] for i in range(9))
 fig_canvas_agg = None
 df = None
+df_Post = None
 password_attempt = None
 window, parameters = None, load_parameters(PARAMETERS_FILE, DEFAULT_SETTINGS)
 print(parameters)
 while True:
 
     if window is None:
-        window = create_main_window(parameters,password_attempt)
+        window = create_main_window(parameters, password_attempt)
 
     event, values = window.read()
     fname = values[0]
@@ -151,7 +171,7 @@ while True:
         window.find_element('baseline').Update(disabled=False)
 
         fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
-        fig.add_subplot(111, xlabel = 'Time (s)', ylabel = 'Current (S.U)').plot(t, ienv)
+        fig.add_subplot(111, xlabel='Time (s)', ylabel='Current (S.U)').plot(t, ienv)
         fig.suptitle('Results', fontsize=16)
         fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
@@ -161,7 +181,7 @@ while True:
 
         window.find_element('baseline').Update(disabled=True)
         fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
-        fig.add_subplot(111, xlabel = 'Time (s)', ylabel = 'Current' ).plot(t, i)
+        fig.add_subplot(111, xlabel='Time (s)', ylabel='Current').plot(t, i)
 
         fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
@@ -180,35 +200,75 @@ while True:
         fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
     elif event == 'Load':
-        data = Helper.readFile(fname)
-        t,i,f,Imag,Imagfilt,ifilt,ienv,int_ienv,ienv_filtered = major_function( int(parameters['freq_pert']),int(parameters['bandwith_window']),
-                                                                                int(parameters['lpf_bw']),int(parameters['harmonic']),float(parameters['max_time']),
-                                                                                float(parameters['max_width']),float(parameters['sample_rate']),data)
 
-        window.find_element('plot').Update(disabled=False)
-        window.find_element('plot2').Update(disabled=False)
-        window.find_element('plot3').Update(disabled=False)
+        tmp = window['OP1'].get()
 
-        d = {
-            't':t,
-            'i':i,
-            'f':f,
-            'Imag':Imag,
-            'Imagfilt':Imagfilt,
-            'ifilt':ifilt,
-            'ienv':ienv,
-            'int_ienv':int_ienv,
-            'ienv_filtered':ienv_filtered
+        # if default radio button is clicked, returns true for precalc
+        if tmp:
+            data = Helper.readFile(fname, 0)
+
+            t, i, f, Imag, Imagfilt, ifilt, ienv, int_ienv, ienv_filtered = major_function(int(parameters['freq_pert']),
+                                                                                           int(parameters[
+                                                                                                   'bandwith_window']),
+                                                                                           int(parameters['lpf_bw']),
+                                                                                           int(parameters['harmonic']),
+                                                                                           float(
+                                                                                               parameters['max_time']),
+                                                                                           float(
+                                                                                               parameters['max_width']),
+                                                                                           float(parameters[
+                                                                                                     'sample_rate']),
+                                                                                           data)
+
+            window.find_element('plot').Update(disabled=False)
+            window.find_element('plot2').Update(disabled=False)
+            window.find_element('plot3').Update(disabled=False)
+
+            d = {
+                't': t,
+                'i': i,
+                'f': f,
+                'Imag': Imag,
+                'Imagfilt': Imagfilt,
+                'ifilt': ifilt,
+                'ienv': ienv,
+                'int_ienv': int_ienv,
+                'ienv_filtered': ienv_filtered
             }
+            df_Post = pd.DataFrame(d)
+            print(type(df))
+            if df_Post is not None:
+                window.find_element('save').Update(disabled=False)
+        else:
+            df_Post = Helper.readFile(fname, 1)
+            t = df_Post[['t']]
+            i = df_Post[['i']]
+            f = df_Post[['f']]
+            Imag = df_Post[['Imag']]
+            Imagfilt = df_Post[['Imagfilt']]
+            ifilt = df_Post[['ifilt']]
+            ienv = df_Post[['ienv']]
+            int_ienv = df_Post[['int_ienv']]
+            ienv_filtered = df_Post[['ienv_filtered']]
 
-        df = pd.DataFrame(d)
-        print(type(df))
-        if df is not None:
-            window.find_element('save').Update(disabled=False)
+            window.find_element('plot').Update(disabled=False)
+            window.find_element('plot2').Update(disabled=False)
+            window.find_element('plot3').Update(disabled=False)
+
+            if df is not None:
+                window.find_element('save').Update(disabled=False)
 
     elif event == 'save':
+
+        tmp = window['OP2'].get()
         outFile = values['save']
-        Helper.writeFile(outFile, df)
+        if tmp:
+
+            Helper.writeFile(outFile, data, 0)
+
+        else:
+
+            Helper.writeFile(outFile, df_Post, 1)
 
     elif event == 'baseline':
         if (len(xdata) >= 2):
