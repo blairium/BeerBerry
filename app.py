36f6daeb7ba97521f50096157aa94dc58551b58f
Michael <michaelthegraps@gmail.com>
2020-09-02 22:31:06 +1000

    added more plot buttons

--- app.py
+++ app.py
@@ -109,7 +109,9 @@ def create_main_window(parameters, password_attempt):
 
               [sg.Canvas(size=(898, 634), key='-CANVAS-')],
               [sg.Button('plot', disabled=True, ), sg.Button('plot2', disabled=True, ),
-               sg.Button('plot3', disabled=True, ), sg.Button('Define baseline', disabled=True,), sg.Button('Map baseline', disabled=True,),
+               sg.Button('plot3', disabled=True, ), sg.Button('plot4', disabled=True, ),
+               sg.Button('plot5', disabled=True, ),sg.Button('plot6', disabled=True, ), 
+                sg.Button('Define baseline', disabled=True,), sg.Button('Map baseline', disabled=True,),
                sg.FileSaveAs(button_text='save', disabled=True, target='save', enable_events=True, key='save',
                              file_types=(('DATA', '.data'), ('BIN', '.bin'), ('CSV', '.csv'), ('All Files', '*.*'))),
                 sg.Radio('Raw Data', 'RAD2', default=True, font=['Helvetica', 10], key='OP2'),
@@ -203,12 +205,43 @@ while True:
         window.find_element('Define baseline').Update(disabled=True)
 
         fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
-        fig.add_subplot(221).plot(f, Imag,  c = '#40BAD2')
-        fig.add_subplot(223).plot(t, ifilt, c = '#40BAD2')
-        fig.add_subplot(222).plot(f, Imagfilt, c = '#40BAD2')
-        fig.add_subplot(224).plot(t, int_ienv, c = '#40BAD2')
+        fig.add_subplot(111, xlabel='f', ylabel='Imag').plot(f, Imag,  c = '#40BAD2')
 
         fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+    elif event == 'plot4':
+        if fig_canvas_agg:
+            destroy_figure(fig_canvas_agg, toolbar)
+
+        window.find_element('Define baseline').Update(disabled=True)
+
+        fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
+        fig.add_subplot(111, xlabel='Time (s)', ylabel='ifilt').plot(t, ifilt, c = '#40BAD2')
+
+        fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+    elif event == 'plot5':
+        if fig_canvas_agg:
+            destroy_figure(fig_canvas_agg, toolbar)
+
+        window.find_element('Define baseline').Update(disabled=True)
+
+        fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
+        fig.add_subplot(111, xlabel='f', ylabel='Imagfilt').plot(f, Imagfilt, c = '#40BAD2')
+
+        fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+    elif event == 'plot6':
+        if fig_canvas_agg:
+            destroy_figure(fig_canvas_agg, toolbar)
+
+        window.find_element('Define baseline').Update(disabled=True)
+
+        fig = matplotlib.figure.Figure(figsize=(9, 6), dpi=100)
+        fig.add_subplot(111, xlabel='Time (s)', ylabel='int_ienv').plot(t, int_ienv, c = '#40BAD2')
+
+        fig_canvas_agg, toolbar = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
 
     elif event == 'Load':
 
@@ -239,6 +272,9 @@ while True:
                             window.find_element('plot').Update(disabled=False)
                             window.find_element('plot2').Update(disabled=False)
                             window.find_element('plot3').Update(disabled=False)
+                            window.find_element('plot4').Update(disabled=False)
+                            window.find_element('plot5').Update(disabled=False)
+                            window.find_element('plot6').Update(disabled=False)
 
                             d = {
                                 't': t,
@@ -279,6 +315,9 @@ while True:
                 window.find_element('plot').Update(disabled=False)
                 window.find_element('plot2').Update(disabled=False)
                 window.find_element('plot3').Update(disabled=False)
+                window.find_element('plot4').Update(disabled=False)
+                window.find_element('plot5').Update(disabled=False)
+                window.find_element('plot6').Update(disabled=False)
 
                 if df_Post is not None:
                     window.find_element('save').Update(disabled=False)
@@ -335,7 +374,7 @@ while True:
         plt.plot(t,curve_2, c = '#40BAD3')
         plt.plot([t[index_of_peak],t[index_of_peak]],[curve_2[index_of_peak],curve_1[index_of_peak]], c = 'r')
         plt.fill_between(t,curve_1,curve_2, alpha = 0.3)
-        fig.suptitle('Inc Baseline', fontsize=16)
+        fig.suptitle('Results', fontsize=16)
         fig.set_size_inches(9,6)
         fig.set_dpi(100)
         destroy_figure(fig_canvas_agg, toolbar)
